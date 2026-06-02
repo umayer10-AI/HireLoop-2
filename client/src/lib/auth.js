9695855dd495e96cdcc63c5920e0814ff991d0c4
umayer10-AI <mdumayer10@gmail.com>
2026-06-02 15:33:39 +0600

    first commit

---
+++ client/src/lib/auth.js
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MY);
+const db = client.db("myAuth");
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client
+    }),
+    emailAndPassword: { 
+        enabled: true, 
+    },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
+});
